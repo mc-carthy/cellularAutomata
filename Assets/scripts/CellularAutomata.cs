baa1b565d7f42a1401b443ae0074289592887616
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-17 23:19:04 +0000

    Implement smoothing algorithm

--- Assets/scripts/CellularAutomata.cs
+++ Assets/scripts/CellularAutomata.cs
@@ -9,18 +9,30 @@ public class CellularAutomata : MonoBehaviour {
     public int height;
     [RangeAttribute (0, 100)]
     public int randomFillPercent;
+    public int smoothingIterations;
 
 	private int [,] map;
 
     private void Start ()
     {
         GenerateMap ();
+        CentreMap ();
     }
 
     private void GenerateMap ()
     {
         map = new int [width, height];
         RandomFillMap ();
+
+        for (int i = 0; i < smoothingIterations; i++)
+        {
+            SmoothMap ();
+        }
+    }
+
+    private void CentreMap ()
+    {
+        transform.position = new Vector3 (width / 2f, 0, height / 2f);
     }
 
     private void RandomFillMap ()
@@ -36,9 +48,63 @@ public class CellularAutomata : MonoBehaviour {
         {
             for (int y = 0; y < height; y++)
             {
-                map [x, y] = (prng.Next (0, 100) < randomFillPercent) ? 1 : 0;
+                // Ensure walls surround the grid
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map [x, y] = 1;
+                }
+                else
+                {
+                    map [x, y] = (prng.Next (0, 100) < randomFillPercent) ? 1 : 0;
+                }
             }
         }
+    }
+
+    private void SmoothMap ()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount (x, y);
+
+                if (neighbourWallTiles > 4)
+                {
+                    map [x, y] = 1;
+                }
+                else if (neighbourWallTiles < 4)
+                {
+                    map [x, y] = 0;
+                }
+            }
+        }   
+    }
+
+    private int GetSurroundingWallCount (int gridX, int gridY)
+    {
+        int wallCount = 0;
+
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map [neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    // Encourage walls around edge of map
+                    wallCount++;
+                }
+            }   
+        }
+
+        return wallCount;
     }
 
     private void OnDrawGizmos ()
