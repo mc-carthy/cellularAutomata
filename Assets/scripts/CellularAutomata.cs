0672095aaf466f7debb4c7cf627f55f1ba8b4625
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-18 14:03:10 +0000

    Limit size of isolated room/wall regions

--- Assets/scripts/CellularAutomata.cs
+++ Assets/scripts/CellularAutomata.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 [RequireComponent (typeof (MarchingSquaresMeshGen))]
 public class CellularAutomata : MonoBehaviour {
@@ -12,6 +13,8 @@ public class CellularAutomata : MonoBehaviour {
     [RangeAttribute (0, 100)]
     public int randomFillPercent;
     public int smoothingIterations;
+    public int wallThresholdSize;
+    public int roomThresholdSize;
 
 	private int [,] map;
 
@@ -37,6 +40,8 @@ public class CellularAutomata : MonoBehaviour {
         {
             SmoothMap ();
         }
+
+        ProcessMap ();
 
         int [,] borderedMap = new int [width + borderSize * 2, height + borderSize * 2];
 
@@ -91,6 +96,99 @@ public class CellularAutomata : MonoBehaviour {
         }
     }
 
+    private List<Coord> GetRegionTiles (int startX, int startY)
+    {
+        List<Coord> tiles = new List<Coord> ();
+
+        int [,] mapFlags = new int [width, height];
+        int tileType = map [startX, startY];
+
+        Queue<Coord> queue = new Queue<Coord> ();
+        queue.Enqueue (new Coord (startX, startY));
+        mapFlags [startX, startY] = 1;
+
+        while (queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue ();
+            tiles.Add (tile);
+
+            for (int x = tile.tileX - 1; x <= tile.tileX + 1; x++)
+            {
+                for (int y = tile.tileY - 1; y <= tile.tileY + 1; y++)
+                {
+                    if (IsInMapRange (x, y) && (x == tile.tileX || y == tile.tileY))
+                    {
+                        if (mapFlags [x, y] != 1 && map [x, y] == tileType)
+                        {
+                            mapFlags [x, y] = 1;
+                            queue.Enqueue (new Coord (x, y));
+                        }
+                    }
+                }                
+            }
+        }
+
+        return tiles;
+    }
+
+    private List<List<Coord>> GetRegions (int tileType)
+    {
+        List<List<Coord>> regions = new List<List<Coord>> ();
+
+        int [,] mapFlags = new int [width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (mapFlags [x, y] == 0 && map [x, y] == tileType)
+                {
+                    List<Coord> newRegion = GetRegionTiles (x, y);
+                    regions.Add (newRegion);
+
+                    foreach (Coord tile in newRegion)
+                    {
+                        mapFlags [tile.tileX, tile.tileY] = 1;
+                    }
+                }
+            }
+        }
+
+        return regions;
+    }
+
+    private void ProcessMap ()
+    {
+        List<List<Coord>> wallRegions = GetRegions (1);
+        
+        // Remove wall regions smaller than  wallThresholdSize
+        foreach (List<Coord> wallRegion in wallRegions)
+        {
+            if (wallRegion.Count < wallThresholdSize)
+            {
+                foreach (Coord tile in wallRegion)
+                {
+                    map [tile.tileX, tile.tileY] = 0;
+                }
+            }
+        }
+
+        List<List<Coord>> roomRegions = GetRegions (0);
+        
+        // Remove wall regions smaller than  wallThresholdSize
+        foreach (List<Coord> roomRegion in roomRegions)
+        {
+            if (roomRegion.Count < roomThresholdSize)
+            {
+                foreach (Coord tile in roomRegion)
+                {
+                    map [tile.tileX, tile.tileY] = 1;
+                }
+            }
+        }
+    }
+
+
     private void SmoothMap ()
     {
         for (int x = 0; x < width; x++)
@@ -119,7 +217,7 @@ public class CellularAutomata : MonoBehaviour {
         {
             for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
             {
-                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                if (IsInMapRange (neighbourX, neighbourY))
                 {
                     if (neighbourX != gridX || neighbourY != gridY)
                     {
@@ -137,20 +235,20 @@ public class CellularAutomata : MonoBehaviour {
         return wallCount;
     }
 
-    // private void OnDrawGizmos ()
-    // {
-    //     if (map != null)
-    //     {
-    //         for (int x = 0; x < width; x++)
-    //         {
-    //             for (int y = 0; y < height; y++)
-    //             {
-    //                 Gizmos.color = (map [x, y] == 1) ? Color.black : Color.white;
-    //                 Vector3 pos = new Vector3 (-width / 2 + x + 0.5f, 0, -height / 2 + y + 0.5f);
-    //                 Gizmos.DrawCube (pos, Vector3.one);
-    //             }
-    //         }
-    //     }
-    // }
+    private bool IsInMapRange (int x, int y)
+    {
+        return x > 0 && x < width && y > 0 && y < height;
+    }
+
+    struct Coord {
+        public int tileX;
+        public int tileY;
+
+        public Coord (int x, int y)
+        {
+            tileX = x;
+            tileY = y;
+        }
+    }
 
 }
